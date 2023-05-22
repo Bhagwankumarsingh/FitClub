02d35053b3f8a641ff9cc30113ac7817ef083ba1
Bhagwan Kumar Singh <bs5117300@gmail.com>
2023-05-23 01:17:30 +0530

    first

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -7,18 +7,18 @@ export const testimonialsData = [
     image: image1,
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
+    name: 'Ishant Singh Rathore',
     status : 'ENTREPRENEUR'
   },
   {
     image: image2,
-    review: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi ipsam, ab itaque nam perferendis impedit sint ',
+    review: 'In the terms of Trainer Fitclub Has a well-trained Trainer who motivate you to workout with Proper-Diet ',
     name: 'JOHN KEVIN',
     status: 'COACH'
   },
   {
     image : image3,
-    review:' Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima aspernatur quod voluptatem',
+    review:' one of the greatest dicision i have made ever in the life Thank you Fitclub',
     name: 'FRANKLIN',
     status: "CUSTOMER"
   }
